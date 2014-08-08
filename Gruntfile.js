e52f57bfa68b7737460881987cfc7f5f38cb58ca
Dave Cranwell <david@torchbox.com>
2014-08-08 12:19:53 +0100

    first commmit

---
+++ Gruntfile.js
@@ -0,0 +1,144 @@
+'use strict';
+
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        stagingConfig: {},
+
+        // Run a connect serer with the livereload script injected
+        connect: {
+            server: {
+                options: {
+                    port: 9001,
+                    livereload: true
+                }
+            }
+        },
+
+        // opens a browser window to the site
+        open: {
+            dev: {
+                path: 'http://localhost:<%= connect.server.options.port %>/site/build/'
+            }
+        },
+
+        // provides server side include support
+        ssi: {
+            options: {
+                cache:'all',
+                baseDir:'site/src'
+            },
+            build:{
+                files: [{
+                    expand: true,
+                    cwd: 'site/src',
+                    src: ['**/*.html'],
+                    dest: 'site/build',
+                }]
+            }
+        },
+
+        // compiles sass
+        sass: {
+            dist: {
+                options: {
+                    style: 'expanded'
+                },
+                files: {
+                    'site/build/css/main.css': 'site/src/css/main.scss' // 'destination': 'source'
+                }
+            }
+        },
+
+        // Copies the contents of src into build
+        copy: {
+            js:{
+                expand: true,
+                cwd: 'site/src', 
+                src: ['js/**/*'], 
+                dest: 'site/build/'
+            },
+            build: {
+                expand: true,
+                cwd: 'site/src', 
+                src: ['**'], 
+                dest: 'site/build/'
+            }
+        },
+
+        // Observe the js/html/css/sass files for changes and execute the tasks
+        watch: {
+            options: { livereload: true },
+            html: {
+                files: 'site/src/**/*.html',
+                tasks: ['ssi']
+            },
+            scripts: {
+                files: 'site/src/**/*.js',
+                tasks: ['copy:js']
+            },
+            sass: {
+                files: 'site/src/**/*.scss',
+                tasks: ['sass']
+            }
+        },
+
+        sshexec: {
+            options: {
+                host: "<%= stagingConfig.host %>",
+                username: "<%= stagingConfig.username %>",
+                privateKey: '<%= stagingConfig.localKey %>',
+                passphrase: '<%= stagingConfig.passphrase %>'
+            },
+            stagingprep: {
+                command: "umask 002 && rm -rf <%= stagingConfig.destinationPath %> && mkdir <%= stagingConfig.destinationPath %>"
+            },
+            stagingchmod:{
+                command: "chmod -R 775 <%= stagingConfig.destinationPath %> "
+            }
+        },
+
+        sftp:{
+            staging: {
+                options: {
+                    host: '<%= stagingConfig.host %>',
+                    username: '<%= stagingConfig.username %>',
+                    privateKey: '<%= stagingConfig.localKey %>',
+                    passphrase: '<%= stagingConfig.passphrase %>',
+                    path: '<%= stagingConfig.destinationPath %>',
+                    srcBasePath: "site/build/",
+                    showProgress: true,
+                    createDirectories:true
+                },
+                files: {
+                    "./": ["site/build/**/*"]
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-open');
+    grunt.loadNpmTasks('grunt-ssi');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-ssh');
+  
+    grunt.registerTask('default', 'Runs a local development server and opens the site in a new browser page/tab', 
+        ['copy:build', 'sass', 'connect', 'open:dev', 'ssi', 'watch']
+    );
+
+    grunt.registerTask('stage', 'Uploads site to staging server', function(){
+        var settingsFile = "staging-config.json"
+        if(!grunt.file.exists(settingsFile)){
+            grunt.log.error('Your staging-config.json file was not found. Copy it from staging-config.example.json')
+            return false
+        }else{
+            var stagingConfig = grunt.file.readJSON(settingsFile)
+        }
+        grunt.config.set('stagingConfig', stagingConfig);
+        grunt.config.set('stagingConfig.localKey', grunt.file.read(stagingConfig.localKeyPath))
+        grunt.task.run(['copy:build', 'sass', 'ssi', 'sshexec:stagingprep', 'sftp', 'sshexec:stagingchmod'])
+    });
+};
---
+++ README.md
@@ -0,0 +1,94 @@
+Design In The Browser Bootstrap
+===============
+
+This README is written in Markdown, so is best read here:
+https://github.com/torchbox/design-in-browser-bootstrap/blob/master/README.md
+
+
+What it provides
+----------------
+
+* Server Side Include (SSI) support but *without* a need for any serverside language. This makes it easy to create modular code.
+* SASS compilation support.
+* An automated way to upload your changes to a staging server.
+
+
+Installation
+----------------
+
+* Install Node.js: Click on the big "INSTALL" button here http://nodejs.org
+* Install Grunt: `npm install -g grunt` (may require sudo)
+* Install SASS: `sudo gem install sass` (may require sudo)
+* Install this project's dependencies: Run `npm install` (Do *not* use sudo)
+
+
+Development
+----------------
+
+* The files you will want to edit are in `site/src`. A `site/build` folder is created on the fly as part of the server process, but should be ignored.
+
+
+Running the development server
+----------------
+
+* Run `grunt` from the command line from the same directory as this README.
+
+
+Preparing to deploy to a staging server (Once ever per developer computer)
+----------------
+
+The following only needs to be done on every machine you develop on, but only once per machine - not once per project.
+
+* SSH into playground via terminal
+* Run `cat ~/.ssh/authorized_keys`. 
+
+If you know what you're doing and your public key is already in it, skip this section. Otherwise, if "No such file or directory":
+
+* On Playground run: `mkdir ~/.ssh/ && touch ~/.ssh/authorized_keys && chmod -R 700 ~/.ssh`
+* On your local machine run: `cat ~/.ssh/id_rsa.pub`
+
+If "No such file or directory":
+
+* On your local machine run: `ssh-keygen -t rsa -C "[your torchbox email address]"` (replacing the square bracket placeholder, duh). Hit enter every time you're asked a question. This generates your keys.
+
+Once you've generated keys, or if you've generated them at some point in the past, we need to install them on playground. This requires the "Homebrew" package manager for OSX. 
+
+(If typing "brew" on your command line indicates the command doesn't exist, you need to install Brew using the instructions here: http://brew.sh/. )
+
+* With Brew installed, on local run: `ssh-copy-id [your-ldap-username]@playground`
+
+This will automatically copy your key from your local machine to playground. Double check it works with a simple login test:
+
+* On local run: `ssh [your-ldap-username]@playground`
+
+Successfull key installation is indicated by it logging you in WITHOUT asking for a password. That was only a test though, so you can exit again.
+
+
+Preparing to deploy to a staging server (For each project)
+----------------
+
+The following needs to be done for this project specifically
+
+* Copy the file `staging-config.example.json` (found in the same dir as this README), to `staging-config.json`
+
+Change the contents of the new file: 
+
+* The `username` should be the one you usually use to connect to the staging server/playground. 
+* `destinationPath` is the path on the server. e.g  `/var/www/my-new-site/`. This path must end with a slash.
+* `localKeyPath` is the path to the *private* half of the key you created above. If in doubt, on OSX it should read: `/Users/[your osx username]/.ssh/id_rsa`
+* If you've password protected your key, `passphrase` should contain the password you used.
+
+
+Deploying to staging
+----------------
+
+* Run `grunt stage`
+* Your site should now be available at http://your-server/[whatever folder name you gave in staging-config.json]
+
+Be warned: this completely replaces the previous version. If you want to deploy it to a new directory, just change the directory name in `staging-config.json` first.
+
+
+Troubleshooting
+----------------
+
+Grunt copies the contents of `site/src` to `site/build` each time it runs, to ensure all the source files are available to the grunt server, which runs from the `site/build` directory. Any *new* files you create while the server is running won't be available in the `src/build` folder until you stop and restart the grunt server.
