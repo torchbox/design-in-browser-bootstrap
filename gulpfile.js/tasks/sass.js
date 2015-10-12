bd4514bcc5bcbb627b02ca8fc6c3d8013e085a36
Dave Cranwell <david@torchbox.com>
2015-10-12 17:38:54 +0100

    minor reformatting

--- gulpfile.js/tasks/sass.js
+++ gulpfile.js/tasks/sass.js
@@ -2,16 +2,18 @@ var gulp         = require('gulp');
 var browserSync  = require('browser-sync');
 var sass         = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
-var config       = require('../config').sass;
 var notify       = require('gulp-notify');
+var config       = require('../config').sass;
 
 gulp.task('sass', function () {
     return gulp.src(config.src)
-        .pipe(sass(config.settings).on('error', notify.onError({
-            title: 'SASS compilation error',
-            message: '<%= error.message %>', 
-            time: 10000,
-        })))
+        .pipe(sass(config.settings)
+            .on('error', notify.onError({
+                title: 'SASS compilation error',
+                message: '<%= error.message %>', 
+                time: 10000,
+            }))
+        )
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
