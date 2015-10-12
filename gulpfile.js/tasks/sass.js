3164a6dd4e0abea88834e090ec734479ea704820
Dave Cranwell <david@torchbox.com>
2015-10-12 17:34:15 +0100

    added desktop growl/toast notification

--- gulpfile.js/tasks/sass.js
+++ gulpfile.js/tasks/sass.js
@@ -3,10 +3,15 @@ var browserSync  = require('browser-sync');
 var sass         = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var config       = require('../config').sass;
+var notify       = require('gulp-notify');
 
 gulp.task('sass', function () {
     return gulp.src(config.src)
-        .pipe(sass(config.settings).on('error', sass.logError))
+        .pipe(sass(config.settings).on('error', notify.onError({
+            title: 'SASS compilation error',
+            message: '<%= error.message %>', 
+            time: 10000,
+        })))
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
