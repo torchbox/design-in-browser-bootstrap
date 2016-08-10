7797571a7c78b587f26434725ec4a259e74aea20
Toby Brancher <toby@torchbox.com>
2016-08-10 23:40:04 +0100

    Add JS tooling
     - Added babel and config
     - Added eslint and config
     - Added JS lint and build scripts
     - Added test ES6 file with console.log
     - Improved script order and added lint to build
     - updated base.html
     - improved scss linting

--- site/javascript/main.js
+++ site/javascript/main.js
@@ -1,4 +1,23 @@
-$(function(){
-	
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    toString() {
+        return `(${this.x}, ${this.y})`;
+    }
+}
 
-});	
+class ColorPoint extends Point {
+    constructor(x, y, color) {
+        super(x, y);
+        this.color = color;
+    }
+    toString() {
+        return super.toString() + " in " + this.color;
+    }
+}
+
+const cp = new ColorPoint(25, 8, "green");
+
+console.log( "hello", cp );
