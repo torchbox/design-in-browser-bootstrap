495af2eb7677c5a97d4aa1e8dac60f22c3b29f9b
Toby Brancher <email@tobybrancher.net>
2017-03-20 00:25:05 +0000

    WIP ES6 Classes, Utils, Navigation, rename page__ to app__
     - WIP to continue later
     - Showcases friendly, simple usage of ES6 classes and utility functions with module loading
     - Showcase managing options with default elegantly
     - Showcase useful javascript utility function for enhancing animations (intro, outro, unload, callback)

--- site/javascript/main.js
+++ site/javascript/main.js
@@ -1,62 +1,10 @@
-import $ from './globals';
+import MobileMenu from './components/MobileMenu';
 
 
-function checkSticky() {
+let mobileNavigation = new MobileMenu({
+    target      : '#app__nav',
+    inSpeed     : 900,
+    outSpeed    : 900
+});
 
-    var $stickyItems = $( '.js-sticky' ),
-        resizeTimer;
-
-    function checkStuck( $item, scrollPos ){
-
-        if( scrollPos >= $item.stickyPos && !$item.stuck ){
-            $item.addClass( 'stuck' );
-            $item.trigger( 'stuck' );
-            $item.stuck = true;
-        } else if( scrollPos < $item.stickyPos && $item.stuck ) {
-            $item.removeClass( 'stuck' );
-            $item.trigger( 'unstuck' );
-            $item.stuck = false;
-        }
-
-    }
-
-    function updateValues( $item ){
-
-        var offset  = +$item.css( 'top' ).split( 'px' )[0], // get top value used, make sure it is a number
-            topPos  = $item.offset().top, // get top position (even if it is stuck, it preserves offset)
-            stuckAt = topPos - offset; // where the element will actually stick
-
-        $item.stickyPos = stuckAt;
-
-    }
-
-    $stickyItems.each(function( e ){
-
-        var $item = $( this );
-
-        $item.stuck = false;    // default to unstuck
-        updateValues( $item );  // update vars
-
-        $( window ).on( 'load', function( e ){
-            checkStuck( $item, e.currentTarget.scrollY );
-        });
-
-        $( window ).on( 'scroll', function( e ){
-            checkStuck( $item, e.currentTarget.scrollY );
-        });
-
-        $( window ).on( 'resize', function( e ){
-            clearTimeout( resizeTimer );
-            resizeTimer = setTimeout(function(){
-
-                updateValues( $item );
-                checkStuck( $item, e.currentTarget.scrollY );
-
-            }, 100 );
-        });
-
-    });
-
-}
-
-checkSticky();
+console.log( 'hello', mobileNavigation );
