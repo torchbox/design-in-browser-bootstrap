7b7f3db4323e6a581dcd62bf2fa4addae94cd9c3
Toby Brancher <email@tobybrancher.net>
2017-09-28 14:21:43 +0100

    Testing globbing and tilde on component based JS

---
+++ site/patterns/03-templates/__app.js
@@ -0,0 +1,19 @@
+/*
+	The architecture for this file is still a WIP
+
+	Please review and consider the approach and ensure it works 
+	for your project if you plan on copying and pasting this 
+	file and layout.
+*/
+
+// import $ from '../../../javascript/globals';
+import $ from '~/site/javascript/globals';
+
+//
+class App {
+    constructor() {
+        // document.getElementsByClassName( 'app' )[0];
+    }
+}
+
+new App();
