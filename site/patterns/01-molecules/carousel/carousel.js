f7493b6744ba2333f6f39d0dfd390b5183477fd4
Simon Evans <simon.evans@torchbox.com>
2017-11-22 23:26:13 +0000

    Add styles and dynamic pager

--- site/patterns/01-molecules/carousel/carousel.js
+++ site/patterns/01-molecules/carousel/carousel.js
@@ -12,25 +12,50 @@ class Carousel {
     constructor() {
 
         // Set carousel elements
-        this.carouselName = '.carousel';
-        this.prev = document.querySelector('.carousel__prev');
-        this.next = document.querySelector('.carousel__next');
-        this.paginationContainer = document.querySelector('.carousel__pagination');
-        this.paginationButton = 'carousel__button';
+        this.carouselClass = '.carousel';
+        this.prev = document.querySelector('.carousel__navigation-prev');
+        this.next = document.querySelector('.carousel__navigation-next');
+        this.pagerContainer = document.querySelector('.carousel__pager');
+        this.pagerContainerInit = 'carousel__pager--init';
+        this.pagerButton = 'carousel__pager-button';
+        this.pagerButtonActive = 'carousel__pager-button--active';
         this.duration = 500;
-
+        this.easing = 'cubic-bezier(0.65, 0.05, 0.35, 1)';
+        
         this.bindEvents();
     }
 
     runCarousel() {
+
+        // Set carousel config
         this.carousel = new Siema({
-            selector: this.carouselName,
+            selector: this.carouselClass,
             duration: this.duration,
-            easing: 'cubic-bezier(0.65, 0.05, 0.35, 1)'
+            easing: this.easing,
+            onChange: function() {
+                this.updatePager();
+            }
         });
+
+        // Pass pager config
+        let pager = this.pagerContainer;
+        let pagerInit = this.pagerContainerInit;
+        let pagerButtonActive = this.pagerButtonActive;
+
+        // Update pager
+        Siema.prototype.updatePager = function() {
+
+            // Remove default first item active state
+            pager.classList.remove(pagerInit);
+
+            for (let i = 0; i < this.innerElements.length; i++) {
+                const addOrRemove = this.currentSlide === i ? 'add' : 'remove';
+                pager.childNodes[i].classList[addOrRemove](pagerButtonActive);
+            }
+        }
     }
 
-    addCarouselControls() {
+    addControls() {
 
         // Button navigation
         this.prev.addEventListener('click', () => this.carousel.prev());
@@ -41,37 +66,39 @@ class Carousel {
 
             // Left arrow
             if (e.keyCode === 37) {
-                this.carousel.prev()
+                this.carousel.prev();
             }
 
             // Right arrow
             else if (e.keyCode === 39) {
-                this.carousel.next()
+                this.carousel.next();
             }
         });
     }
 
-    addCarouselPagination() {
-        let pagination = this.paginationContainer;
-        let paginationButton = this.paginationButton;
+    addPager() {
 
-        Siema.prototype.addPagination = function() {
+        // Pass pager config
+        let pager = this.pagerContainer;
+        let pagerButton = this.pagerButton;
+
+        Siema.prototype.addPager = function() {
             for (let i = 0; i < this.innerElements.length; i++) {
                 let button = document.createElement('button');
-                button.className = paginationButton;
+                button.className = pagerButton;
                 button.textContent = i;
                 button.addEventListener('click', () => this.goTo(i));
-                pagination.appendChild(button);
+                pager.appendChild(button);
             }
         }
 
-        this.carousel.addPagination();
+        this.carousel.addPager();
     }
 
     bindEvents() {
         this.runCarousel();
-        this.addCarouselControls();
-        this.addCarouselPagination();
+        this.addControls();
+        this.addPager();
     }
 }
 
