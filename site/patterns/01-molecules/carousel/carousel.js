d2f1367f6bbd655595838c2543562ef4ae989ef0
Simon Evans <simon.evans@torchbox.com>
2017-11-22 21:41:29 +0000

    Add keyboard controls

--- site/patterns/01-molecules/carousel/carousel.js
+++ site/patterns/01-molecules/carousel/carousel.js
@@ -2,13 +2,36 @@ import Siema from 'siema';
 
 class Carousel {
     constructor() {
-        this.runCarousel();
+        this.carousel = new Siema();
+        this.prev = document.querySelector('.prev');
+        this.next = document.querySelector('.next');
+        this.bindEvents();
     }
 
-    runCarousel() {
-        const carousel = new Siema();
-        document.querySelector('.prev').addEventListener('click', () => carousel.prev());
-        document.querySelector('.next').addEventListener('click', () => carousel.next());
+    runCarouselControls() {
+
+        // Button navigation
+        this.prev.addEventListener('click', () => this.carousel.prev());
+        this.next.addEventListener('click', () => this.carousel.next());
+
+        // Keyboard navigation
+        document.addEventListener('keydown', (e) => {
+
+            // Left arrow key
+            if (e.keyCode === 37) {
+                this.carousel.prev()
+            }
+
+            // Right arrow key
+            else if (e.keyCode === 39) {
+                this.carousel.next()
+            }
+        });
+    }
+
+    bindEvents() {
+        this.runCarousel();
+        this.runCarouselControls();
     }
 }
 
